051ea4049a7d4708d152c34873d9d53515478573
Akili Humphrey <akilihumphrey@gmail.com>
2019-02-20 08:34:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,12 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/team'
-    "Coach: #{params :coach}"
-    "Point Gaurd: #{params :pg}"
-    "Shooting Gaurd: #{params :sg}"
-    "Small Forward: #{params :sf}"
-    "Power Forward #{params :pf}"
-    "Center: #{params :c}"
+    @team = "Coach: #{params :coach}" "Point Gaurd: #{params :pg}" "Shooting Gaurd: #{params :sg}" "Small Forward: #{params :sf}" "Power Forward #{params :pf}" "Center: #{params :c}"
     erb :team 
   end
 
